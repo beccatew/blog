a3e7eb1cfdc3bed25133a5fc6fdee0dfb599ce97
Becca Tew <rebeccatew@gmail.com>
2018-02-26 13:50:01 -0700

    update/edit page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,49 @@
+class PagesController < ApplicationController
+# GET /pages
+  def index
+    @pages = Page.all
+  end
+
+# GET /pages/:id
+  def show
+    @page = Page.find(params[:id])
+  end
+
+# GET /pages/new
+  def new
+    @page = Page.new
+  end
+
+#GET /pages/:id/edit
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+# POST /pages
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  # PUT / PATCH /pages/:id
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
+  end 
+
+  private
+    def page_params
+      params.require(:page).permit(:body, :author, :title)
+    end
+
+end
